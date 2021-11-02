f8e345cb78b24c2bc3bc36fccf2ee7ce3b6b9551
sahinur31 <sahinur4841@gmail.com>
2021-11-02 12:28:08 +0600

    jwt

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
+var admin = require("firebase-admin");
 
 const app = express();
 const port = process.env.PORT || 5000;
+// firebase admin initialization 
+var serviceAccount = require('./ema-john-ph-firebase-adminsdk-14p82-50aadfd44b.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 // middleware
 app.use(cors());
@@ -16,12 +23,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
+
+async function verifyToken(req, res, next) {
+  // console.log(req.headers);
+  if (req.headers.authorization.startsWith('Bearer ')) {
+      const idToken = req.headers.authorization.split('Bearer ')[1];
+      try {
+          const decodedUser = await admin.auth().verifyIdToken(idToken);
+          req.decodedUserEmail = decodedUser.email;
+      }
+      catch {
+
+      }
+  }
+  next();
+}
+
+
 async function run() {
   try {
     await client.connect();
     const database = client.db("online-shop");
     const productCollection = database.collection("product");
-    const orderCollection = database.collection('orders');
+    const orderCollection = database.collection("orders");
 
     // get product api
     app.get("/products", async (req, res) => {
@@ -44,23 +68,43 @@ async function run() {
       });
     });
     // Use POST to get data by keys
-    app.post('/products/byKeys', async (req, res) => {
-        const keys = req.body;
-        const query = { key: { $in: keys } }
-        const products = await productCollection.find(query).toArray();
-        res.json(products);
+    app.post("/products/byKeys", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productCollection.find(query).toArray();
+      res.json(products);
     });
 
     // Add Orders API
-    app.post('/orders', async (req, res) => {
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.json(result);
-    })
-    
+    /* app.get("/orders", async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if(email) {
+          query = { email: email };
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    }); */
+    app.get('/orders', verifyToken, async (req, res) => {
+      const email = req.query.email;
+      if (req.decodedUserEmail === email) {
+          const query = { email: email };
+          const cursor = orderCollection.find(query);
+          const orders = await cursor.toArray();
+          res.json(orders);
+      }
+      else {
+          res.status(401).json({ message: 'User not authorized' })
+      }
 
-
-
+  });
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      order.createdAt = new Date();
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
